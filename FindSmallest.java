b43c060f1c8264b1cd1d30c7281dbff1501d6f45
Keerthika1607 <Keerthika.thiru16072003@gmail.com>
2025-04-03 14:44:50 +0530

    Day - 3 Array Problems

---
+++ FindSmallest.java
@@ -0,0 +1,13 @@
+public class FindSmallest{
+  public static void main(String args[]){
+
+     int arr[] = {12, 13, 1, 10, 34, 10};
+     int min = arr[0];
+     for(int i=0; i<arr.length; i++) { 
+      if(min > arr[i]){
+            min = arr[i];
+         }
+     }
+     System.out.print(min); 
+  }
+}
---
+++ ReverseArray.java
@@ -0,0 +1,35 @@
+public class ReverseArray {
+   public static void main(String args[])
+   {
+
+     int arr[] = {10, 20, 30, 40, 50};
+
+     int n=arr.length;
+     for(int i=n-1; i>=0; i--)
+       System.out.print(arr[i]+" "); 
+    }
+}
+
+/*
+ * 
+ * public class Main{
+  public static void main(String args[])
+  {
+
+     int arr[] = {10, 20, 30, 40, 50};
+
+     int n=arr.length;
+     int start = 0, end = n-1;
+    
+     while(start<end){
+        int temp = arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+        start++;
+        end--;
+     }
+    for(int i=0; i<n; i++)
+       System.out.print(arr[i]+" "); 
+    }
+ }
+ */
